a2fec8ade914dfc7e8630426d5cee457514b8c5a
Biel CASTANO SEGADE <castano@gpuclust01.bioclust.biologie.ens.fr>
2024-08-09 16:07:46 +0200

    Added parametrized port to the training script

--- train_jeanzay.sh
+++ train_jeanzay.sh
@@ -1,3 +1,4 @@
+  GNU nano 5.6.1                                                                                                                                                                         train_jeanzay.sh                                                                                                                                                                                   
 #!/bin/bash
 # Usage:
 # ./train_two_classes.sh gpu_list (like 0,1,2 or 0) "data_subfolder1" "data_subfolder2" "method" batch_size (like 128) model_index (0 for nano, 1 for stable-diffusion)
@@ -10,10 +11,11 @@ export METHOD=${4:-"attention"}
 export BATCH_SIZE=${5:-64}
 export DATA_SAMPLES=${6:-10}
 export DATA_SAMPLING_SEED=${7:-43}
+export MAIN_PROCESS_PORT=${8:-24591}
 
 MODEL_NAMES=("/lustre/fsn1/projects/rech/arj/uni93xz/base_outputs/stable-diffusion-nano-2-1/local/checkpoints/" "/lustre/fsn1/projects/rech/arj/uni93xz/base_outputs/stable-diffusion-2-1")
 
-MODEL_INDEX=${8:-0} 
+MODEL_INDEX=${9:-0} 
 export MODEL_NAME=${MODEL_NAMES[$MODEL_INDEX]}
 
 # To remove intermediate folders
@@ -32,7 +34,7 @@ export OUTPUT_DIR="${BASE_OUTPUT_DIR}${EXPERIMENT_NAME}"
 
 
 # Construct the base accelerate launch command
-CMD="accelerate launch --main_process_port 24591 --num_processes $NUM_PROCESSES --multi_gpu --num_machines 1 --mixed-precision no --dynamo_backend no"
+CMD="accelerate launch --main_process_port ${MAIN_PROCESS_PORT} --num_processes $NUM_PROCESSES --multi_gpu --num_machines 1 --mixed-precision no --dynamo_backend no"
 
 # Execute the command with the common options
 $CMD /gpfswork/rech/arj/uni93xz/stablediffusion_nano/scripts/accelerate_train.py \
@@ -41,19 +43,23 @@ $CMD /gpfswork/rech/arj/uni93xz/stablediffusion_nano/scripts/accelerate_train.py
   --output_dir=$OUTPUT_DIR \
   --resolution=128 \
   --train_batch_size=$BATCH_SIZE \
+  --checkpointing_steps=10000 \
   --gradient_accumulation_steps=1 \
   --learning_rate=1e-3 \
   --lr_scheduler="cosine" \
   --lr_warmup_steps=0 \
   --report_to="wandb" \
-  --validation_epochs=20 \
+  --validation_epochs=1000 \
   --num_validation_images=32 \
   --num_inference_steps=100 \
   --experiment_name=$EXPERIMENT_NAME \
   --validation_batch_size=200 \
   --data_sampling_seed=$DATA_SAMPLING_SEED \
   --data_samples=$DATA_SAMPLES \
-  --max_train_steps=10000 \
+  --max_train_steps=30000 \
   --use_local_checkpoints \
   --inception_weights_path="/lustre/fsn1/projects/rech/arj/uni93xz/base_outputs/inception/weights-inception-2015-12-05-6726825d.pth" \
   --finetunning_method=$METHOD
+
+
+
